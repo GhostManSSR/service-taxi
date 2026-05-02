92c89916d9e9db4753ee30574b8053b9a4c9d650
Vladimir070142 <artem.ivan456@gmail.com>
2026-05-02 14:38:21 +0700

    Add Entity for bd

---
+++ user-service/src/main/java/user_api/entity/Passenger.java
@@ -0,0 +1,23 @@
+package user_api.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "passengers")
+@Data
+public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+
+    private LocalDateTime createdAt;
+}
+
