32021a7bd926ac38e466da87713eb9cfc7565ed8
Vladimir070142 <artem.ivan456@gmail.com>
2026-05-02 22:59:52 +0700

    Update docker and setting project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
 rootProject.name = "microservices-project"
 
-include("notification-service", "trip-service", "user-service")
+include("notification-service", "trip-service", "user-service", "api-gateway")
